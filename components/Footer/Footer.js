3812d104d26d5741b1d70491999c24272808e69e
CollinsRollinsDev <collinsrollins07@gmail.com>
2021-11-02 12:36:31 -0700

    update

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <section className={styles.first}>
         <p className={styles.head}>Quick Links</p>
         <div className={styles.listDiv}>Home</div>
-        <div className={styles.listDiv}>Wanna Contact us via phone? <a href="tel:9207868519"> Click here</a></div>
+        <div className={styles.listDiv}>Wanna Contact us via phone? <a href="tel:+9207868519"> Click here</a></div>
         <div className={styles.listDiv}>Wanna Contact us via email? <a href = "mailto:florinastrong162@gmail.com"> Click here</a> </div>
       </section>
     </section>
